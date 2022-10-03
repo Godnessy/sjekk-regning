cbe14776c22ab7a25273796e9d7dc9b4d210edef
Godnessy <godnessy@gmail.com>
2022-10-03 18:37:47 +0200

    Added firebase, added functionality for month choice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Papa, { parse } from "papaparse";
 import db from "./firebase-config";
+import prices from "./augustJSON.json";
 
+/*TODO
+Backend:
+- Every day import data from API 
+- Parse the data into this format:
+Check which month this data is for then add it:
+Day:
+Hour: Price
+Day: 
+Hour: Price
+
+-POST into the correct month, if the month does not exist create it.
+
+Frontend:
+- Get CSV from client -
+- Parse the CSV into a format that I can work with
+- GET the relevant month out of the DB
+- Cross the info from the parsed CSV and the DB 
+- Calculate price for every hour of every day and save the results into a new object
+- Display the output into the UI with the design made earlier
+- Add a "send til Epost med PDF" and "Download PDF" button
+-
+
+*/
 const allowedExtensions = ["csv"];
 
 function App() {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
   const [file, setFile] = useState("");
+  const [monthList, setMonthList] = useState();
+  const [usageData, setUsageData] = useState();
+  const [priceData, setPriceData] = useState(prices);
+  const [selectedMonth, setSelectedMonth] = useState();
 
   const handleFileChange = (e) => {
     setError("");
@@ -27,10 +55,40 @@ function App() {
       const parsedData = csv?.data;
       const columns = parsedData;
       setData(columns);
-      console.log(columns);
     };
     reader.readAsText(file);
   };
+
+  const createMonthList = () => {
+    const date = new Date();
+    const months = 12;
+    const currentMonth = date.getMonth();
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let monthArr = [];
+    for (let i = 0; i < currentMonth + 1; i++) {
+      const m = date.getMonth();
+      monthArr.push(monthNames[m]);
+      date.setMonth(date.getMonth() - 1);
+    }
+    return setMonthList(monthArr);
+  };
+
+  useEffect(() => {
+    createMonthList();
+  }, []);
 
   return (
     <>
@@ -44,19 +102,29 @@ function App() {
         name="file"
         type="File"
       />
-      <label htmlFor="pricesInput" style={{ display: "block" }}>
-        enter prices CSV file
-      </label>
 
-      <input
-        onChange={handleFileChange}
-        id="pricesInput"
-        name="file"
-        type="File"
-      />
       <div>
         <button onClick={handleParse}>Parse</button>
       </div>
+
+      <label htmlfor="months"> Choose a month: </label>
+      <select
+        name="months"
+        id="months"
+        value={selectedMonth}
+        onChange={(e) => {
+          setSelectedMonth(e.target.value);
+          console.log(e.target.value);
+        }}
+      >
+        <option value=""> Valg en måned </option>
+        {monthList &&
+          monthList.map((month) => (
+            <option key={month} value={month}>
+              {month}
+            </option>
+          ))}
+      </select>
       {error
         ? error
         : data.map((col, idx) => {
@@ -73,14 +141,16 @@ function App() {
                 </thead>
                 <tbody>
                   <tr>
-                    <td> {values[0].split(" ")[0]}</td>
-                    <td> {values[0].split(" ")[1]}</td>
+                    <td> {values[0].split[0]}</td>
+                    <td> {values[0].split[1]}</td>
                     <td>{values[2]}</td>
                   </tr>
                 </tbody>
               </table>
             );
           })}
+
+      <div>{prices && prices.map((hour) => {})}</div>
     </>
   );
 }
