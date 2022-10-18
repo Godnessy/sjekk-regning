e230a721df2a3d613f0e6cdf2e3c6010a42576fa
Godnessy <godnessy@gmail.com>
2022-10-18 17:16:21 +0200

    fixed private route issue

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <PrivateRoute
+          <Route
             path="/home"
             element={
               <PrivateRoute>
--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,33 +1,10 @@
 import React from "react";
 import { Routes, Route, Redirect, Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Login from "../Pages/Login";
-
-// function PrivateRoute({ component: Component, ...rest }) {
-//   const { currentUser } = useAuth();
-//   return (
-//     <Route
-//       {...rest}
-//       render={({children}) => {
-//         return currentUser ? (
-//         //   <Component {children} />
-//         {children}
-//         ) : (
-//           <Navigate replace to="/login" />
-//         );
-//       }}
-//     ></Route>
-//   );
-// }
 
 function PrivateRoute({ children }) {
   const { currentUser } = useAuth();
-  console.log(children);
-  return currentUser ? (
-    <Route path="/home" />
-  ) : (
-    <Route path="/" element={<Login />} />
-  );
+  return currentUser ? children : <Navigate replace to="/login" />;
 }
 
 export default PrivateRoute;
