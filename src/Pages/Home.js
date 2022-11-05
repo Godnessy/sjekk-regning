75172dc1fa2a886010559233bc46c7b98fe4abec
Godnessy <godnessy@gmail.com>
2022-11-05 18:11:52 +0100

    Reset button for the application

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -316,12 +316,23 @@ function Home() {
                   </div>
                 </div>
                 <div className="calculate-btn">
-                  <button
-                    className="calculate btn btn-success ms-5 my-3 "
-                    onClick={parseCsvJson}
-                  >
-                    Regne ut!
-                  </button>
+                  {totalMonthPrice ? (
+                    <button
+                      className="calculate btn btn-danger ms-5 my-3"
+                      onClick={() => {
+                        window.location.reload();
+                      }}
+                    >
+                      Ny regning
+                    </button>
+                  ) : (
+                    <button
+                      className="calculate btn btn-success ms-5 my-3 "
+                      onClick={parseCsvJson}
+                    >
+                      Regne ut!
+                    </button>
+                  )}
                 </div>
               </div>
               <div className="border fw-bold border-dark border-2">
