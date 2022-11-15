0c3acb5a0a560b4986f6de8f7f612b2d8a43e157
Godnessy <godnessy@gmail.com>
2022-11-15 13:40:47 +0100

    Added parner logo to the navbar and adjusted for different viewports

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import logo from "../Resources/images/logoTransp.png";
+import prisMatch from "../Resources/images/pris-match.png";
 
 function Navbar({ logOut }) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light d-flex flex-row bg-light justify-content-left">
       <a className="navbar-brand" href="#">
         <img src={logo} alt="SjekkRegning.no" className="logo ms-4" />
       </a>
-      {/* <Button className="" onClick={logOut}>
-        Logg ut
-      </Button> */}
+      <p className="cooporation text-decoration-underline fs-5 fw-bold">
+        I samarbeid med:
+      </p>
+      <img src={prisMatch} alt="" className="logo-pris" />
     </nav>
   );
 }
