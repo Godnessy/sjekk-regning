af29a51b37c21b41cdcd434a0b55defb1dd92b39
Godnessy <godnessy@gmail.com>
2022-12-30 13:54:02 +0100

    Temp editing waiting for answer

--- src/Components/ReportError.js
+++ src/Components/ReportError.js
@@ -43,16 +43,19 @@ function ReportError({ uploadFailedFile, file, handleCsvFile }) {
       alert("Vennligst skriv en melding før du sender");
       return;
     }
+    ///Thinking about splitting it into 2 options for docRef depending on email, waiting for answer from Miki
     const date = new Date();
     const dateToSend = date.toLocaleDateString();
     const time = String(date).split(" ")[4];
     const dateForID = dateToSend.replaceAll("/", "-") + "-" + time;
+    const answer = email ? { sentAnswer: false } : null;
     const docRef = await setDoc(doc(db, "userFeedback", dateForID), {
       Message: text,
       Email: email,
+      sentWithFile: sentFile,
       Read: false,
       Implemented: false,
-      sentWithFile: sentFile,
+      answer,
     });
     setText("");
     setEmail("");
