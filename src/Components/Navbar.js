1173025cc80ddc283f6566caa101afc94a8cb97f
Godnessy <godnessy@gmail.com>
2023-01-02 01:19:30 +0100

    Added FB link to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,12 +2,18 @@ import React from "react";
 import logo from "../Resources/images/logoTransp.png";
 import prisMatch from "../Resources/images/pris-match.png";
 import ReportError from "./ReportError";
+import fb from "../Resources/images/fb.png";
 
 function Navbar({ uploadFailedFile, file, handleCsvFile }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light d-flex flex-row bg-light justify-content-left">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand d-flex flex-row" href="#">
         <img src={logo} alt="SjekkRegning.no" className="logo ms-4" />
+        <div className="fb-logo-navbar-container">
+          <a href="https://www.facebook.com/groups/561110268888888">
+            <img src={fb} alt="FB-logo-navbar" />
+          </a>
+        </div>
       </a>
       <div className="report-div">
         {
